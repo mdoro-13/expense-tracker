b5e087f5eb31ddf847868b080e3a8e76ab24e42b
mdoro-13 <mihai.dorobantu.dev@gmail.com>
2022-11-16 23:26:32 +0200

    Prevent access to home unless signed in Issue: 02 Changes: - implement guard that protects routes

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 import { LoggedInGuard } from './guards/logged-in.guard';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'signup', component: SignupComponent, canActivate: [LoggedInGuard] }
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'signup', component: SignupComponent, canActivate: [LoggedInGuard] },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
