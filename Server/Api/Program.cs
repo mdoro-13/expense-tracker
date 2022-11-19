e6473714112db1c1d62a518ba63b2c52d04dd705
mdoro-13 <mihai.dorobantu.dev@gmail.com>
2022-11-19 23:43:19 +0200

    Add authentication scheme Issue: 02

--- Server/Api/Program.cs
+++ Server/Api/Program.cs
@@ -1,4 +1,10 @@
+using FirebaseAdmin;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Server.Authentication;
+
 const string APP_TITLE = "Expense Tracker";
+const string AUTHENTICATION_SCHEME = JwtBearerDefaults.AuthenticationScheme;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +12,12 @@ var config = builder.Configuration;
 
 
 // Add services to the container.
+
+builder.Services.AddSingleton(FirebaseApp.Create());
+
+builder.Services
+    .AddAuthentication(AUTHENTICATION_SCHEME)
+    .AddScheme<AuthenticationSchemeOptions, FirebaseAuthenticationHandler>(AUTHENTICATION_SCHEME, o => { });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
