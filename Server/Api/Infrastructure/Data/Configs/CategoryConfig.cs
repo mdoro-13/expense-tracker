5f299107a57d5ec1cadc2da9849cfb48e12f3dae
mdoro-13 <mihai.dorobantu.dev@gmail.com>
2022-11-21 22:07:57 +0200

    Add entity configs Issue: 05

---
+++ Server/Api/Infrastructure/Data/Configs/CategoryConfig.cs
@@ -0,0 +1,32 @@
+﻿using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Api.Infrastructure.Data.Configs;
+
+public class CategoryConfig : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder
+            .Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder
+            .Property(c => c.Color)
+            .IsRequired()
+            .IsFixedLength()
+            .HasMaxLength(6);
+
+        builder
+            .HasMany(c => c.Expenses)
+            .WithOne(e => e.Category)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        builder
+            .HasMany(c => c.SpendingLimits)
+            .WithOne(sp => sp.Category)
+            .OnDelete(DeleteBehavior.SetNull);
+    }
+}
---
+++ Server/Api/Infrastructure/Data/Configs/ExpenseConfig.cs
@@ -0,0 +1,31 @@
+﻿using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Api.Infrastructure.Data.Configs;
+
+public class ExpenseConfig : IEntityTypeConfiguration<Expense>
+{
+    public void Configure(EntityTypeBuilder<Expense> builder)
+    {
+        builder
+            .Property(e => e.Date)
+            .IsRequired();
+
+        builder
+            .Property(e => e.Amount)
+            .IsRequired();
+
+        builder
+            .Property(e => e.UserId)
+            .IsRequired();
+
+        builder
+            .Property(e => e.CategoryId)
+            .IsRequired(false);
+
+        builder
+            .Property(e => e.Details)
+            .HasMaxLength(200);
+    }
+}
