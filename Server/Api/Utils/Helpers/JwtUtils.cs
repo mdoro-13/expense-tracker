513efa1869a0becd6016e0421276c98f91bd4e9d
mdoro-13 <mihai.dorobantu.dev@gmail.com>
2022-11-27 11:54:09 +0200

    Set up function to get user_id Issue: 06

---
+++ Server/Api/Utils/Helpers/JwtUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+
+namespace Api.Utils.Helpers;
+
+public static class JwtUtils
+{
+    public static string? GetUserId(ClaimsPrincipal user)
+    {
+        var claim = user.Claims.FirstOrDefault(x => x.Type == "user_id");
+
+        if (claim is null)
+        {
+            throw new BadHttpRequestException("No user_id claim found");
+        }
+
+        return claim!.Value;
+    }
+}
