10b0c3ced50267c0262cd45630fc99fc413b5169
mdoro-13 <mihai.dorobantu.dev@gmail.com>
2022-12-06 00:17:21 +0200

    Make expense details nullable Issue: 06 Changes: - added migrations to reflect the change

--- Server/Api/Domain/Entities/Expense.cs
+++ Server/Api/Domain/Entities/Expense.cs
@@ -8,7 +8,7 @@ public class Expense : IEntity<int>, IHasUser<string>
     public int Id { get; set; }
     public decimal Amount { get; set; }
     public DateTime Date { get; set; }
-    public string Details { get; set; }
+    public string? Details { get; set; }
     public string UserId { get; set; }
     public int? CategoryId { get; set; }
     public virtual Category? Category { get; set; }
