a268e7924eb2934dfbbffb867fb8d12d7711b8a1
mdoro-13 <mihai.dorobantu.dev@gmail.com>
2022-12-17 02:40:12 +0000

    Add CalculateBudgetStatsAsync test for not found Issue: 06

---
+++ Server/UnitTests/Services/ExpenseManagerTests.cs
@@ -0,0 +1,36 @@
+﻿using Api.Services;
+using FluentAssertions;
+using Microsoft.Data.Sqlite;
+using System.Security.Claims;
+using UnitTests.Utils;
+using Xunit;
+
+namespace UnitTests.Services;
+
+public sealed class ExpenseManagerTests
+{
+    private const string UserId = "test-id";
+    private readonly ClaimsPrincipal _user;
+
+    public ExpenseManagerTests()
+	{
+        _user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+        {
+            new Claim("user_id", UserId),
+        }, "mock"));
+    }
+
+	[Fact]
+	public async Task CalculateBudgetStatsAsync_Returns_Default_If_No_Budget()
+	{
+        using (var connection = new SqliteConnection("DataSource=:memory:"))
+		{
+			var context = await DbMockUtils.InitializeDbAsync(connection);
+			var sut = new ExpenseManager(context);
+
+			var result = await sut.CalculateBudgetStatsAsync(1, _user);
+
+			result.Should().BeNull();
+		}
+    }
+}
